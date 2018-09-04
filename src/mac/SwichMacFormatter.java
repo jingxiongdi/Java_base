601400e311f016f13c2d265872a1bc856d5b7bd3
jingxiongdi <937969716@qq.com>
2018-09-04 10:18:22 +0800

    测试中文注释

--- src/mac/SwichMacFormatter.java
+++ src/mac/SwichMacFormatter.java
@@ -7,13 +7,13 @@ public class SwichMacFormatter {
 			String mac = "1e:2d:3f:4e:5g:aw";	
 			mac = mac.replace(":", "");
 			System.out.println("mac : "+mac);
-			//输出结果:mac : 1e2d3f4e5gaw
+			//杈撳嚭:mac : 1e2d3f4e5gaw
 			
 			
 			String deviceID = "IOTSHMK000S00090B1B839";
 			deviceID = deviceID.replace("IOTSH", "");
 			System.out.println("deviceid : "+deviceID);
-			//输出结果:deviceid : 1e2d3f4e5gaw
+			//杈撳嚭:deviceid : 1e2d3f4e5gaw
 	}
 
 }
